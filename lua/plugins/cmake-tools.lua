bac5b9f6da9a3096b3329931416138712f3d840e
Marcos Mundina Fabregat <discor2008125@gmail.com>
2026-08-16 15:43:47 +0200

    -j

--- lua/plugins/cmake-tools.lua
+++ lua/plugins/cmake-tools.lua
@@ -4,7 +4,7 @@ return {
     cmake_command = "cmake",
     cmake_build_directory = "build",
     cmake_generate_options = { "-DCMAKE_C_COMPILER=clang", "-DCMAKE_CXX_COMPILER=clang++" },
-    cmake_build_options = {},
+    cmake_build_options = { "-j" },
     cmake_console = {
       split_direction = "horizontal", -- "horizontal" or "vertical"
       split_size = 11,
